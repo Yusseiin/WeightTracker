a9459e06409a29c2226e93ed4f24682de66e9591
Nate Fischer <ntfschr@google.com>
2026-09-15 21:19:16 -0700

    Allow more developer origins
    No production-facing change. This is a developer-focused change to allow
    more address ranges for the developer machine's IP address. I'm adding
    the most common home IP ranges that I'm aware of.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  allowedDevOrigins: ["192.168.1.*", "localhost"],
+  allowedDevOrigins: [
+    "192.168.0.*", "192.168.1.*", "10.0.0.*", "127.0.0.1", "localhost"
+  ],
 };
 
 export default nextConfig;
